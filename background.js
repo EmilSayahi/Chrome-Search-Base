88f02843a6fb30fbf25425b70f0c4a991bdaaa0a
EmilSayahi <emilsayahi@gmail.com>
2015-01-10 15:05:07 -0500

    Added files

---
+++ background.js
@@ -0,0 +1,24 @@
+function resetDefaultSuggestion() {
+  chrome.omnibox.setDefaultSuggestion({
+    description: 'Green Dragon: Search Green Dragon for %s'
+  });
+}
+
+resetDefaultSuggestion();
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+});
+
+chrome.omnibox.onInputCancelled.addListener(function() {
+  resetDefaultSuggestion();
+});
+
+function navigate(url) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url});
+  });
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  navigate("http://greendragontech.tk/Green-Dragon-Search-Beta/#q=" + text);
+});
